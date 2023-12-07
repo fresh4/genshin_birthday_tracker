7edc6491890c6d3e39876dc25cb214272ad23f7f
fresh4 <fresh1736@gmail.com>
2023-12-07 10:43:31 -0600

    testing

--- API/routes.py
+++ API/routes.py
@@ -76,5 +76,5 @@ def send_webhooks():
   return jsonify("Sent webhooks"), 200
 
 if __name__ == "__main__":
-  # app.run(debug=True)
-  serve(app, host="0.0.0.0", port=5000)
+  app.run(debug=True)
+  # serve(app, host="0.0.0.0", port=5000)
